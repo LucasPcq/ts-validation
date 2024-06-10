c58beaef06fe0b9a17f7c21684771f0ceb0592d4
LucasPcq <picquelucas17@gmail.com>
2024-06-10 21:46:33 +0200

    refactor(optional): clean code and separate type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,54 +1,193 @@
 export namespace TSV {
+  /**
+   * Types
+   */
+
   export type TypeString = "string";
   export type TypeNumber = "number";
   export type TypeBoolean = "boolean";
   export type TypeObject = "object";
 
-  export type Type = TypeString | TypeNumber | TypeBoolean | TypeObject;
+  export type TypeOptional = "optional";
+
+  export type Type =
+    | TypeString
+    | TypeNumber
+    | TypeBoolean
+    | TypeObject
+    | TypeOptional;
+
+  /**
+   * Base
+   */
+
+  export type BaseSchema<T extends Type> = {
+    _type: T;
+  };
+
+  /**
+   * Optional
+   */
 
   export type OptionalSchema<
     T extends Type,
-    C extends Record<string, Schema<Type> | OptionalSchema<Type>> = {}
-  > = T extends TypeObject
-    ? {
-        _type: T;
-        isOptional: boolean;
-        children: C;
-      }
-    : {
-        _type: T;
-        isOptional: boolean;
-      };
+    C extends ChildrenObjectSchema = {}
+  > = BaseSchema<TypeOptional> & {
+    child: Omit<Schema<T, C>, "Optional">;
+  };
+
+  export type OptionalMethod<
+    T extends Type,
+    C extends ChildrenObjectSchema = {}
+  > = () => OptionalSchema<T, C>;
+
+  /**
+   * String
+   */
+
+  export type StringSchema = BaseSchema<TypeString> & {
+    Optional: OptionalMethod<TypeString>;
+  };
+
+  /**
+   * Number
+   */
+
+  export type NumberSchema = BaseSchema<TypeNumber> & {
+    Optional: OptionalMethod<TypeNumber>;
+  };
+
+  /**
+   * Boolean
+   */
+
+  export type BooleanSchema = BaseSchema<TypeBoolean> & {
+    Optional: OptionalMethod<TypeBoolean>;
+  };
+
+  /**
+   * Object
+   */
+
+  export type ChildrenObjectSchema = Record<
+    string,
+    Schema<Type> | OptionalSchema<Type>
+  >;
+
+  export type ObjectSchema<C extends ChildrenObjectSchema> =
+    BaseSchema<TypeObject> & {
+      Optional: OptionalMethod<TypeObject, C>;
+      children: C;
+    };
+
+  /**
+   * Schema
+   */
 
   export type Schema<
     T extends Type,
-    C extends Record<string, Schema<Type> | OptionalSchema<Type>> = {}
-  > = T extends TypeObject
-    ? {
-        _type: T;
-        isOptional: boolean;
-        Optional: () => OptionalSchema<T, C>;
-        children: C;
-      }
-    : {
-        _type: T;
-        isOptional: boolean;
-        Optional: () => OptionalSchema<T, C>;
-      };
+    C extends ChildrenObjectSchema = {}
+  > = T extends TypeString
+    ? StringSchema
+    : T extends TypeNumber
+    ? NumberSchema
+    : T extends TypeBoolean
+    ? BooleanSchema
+    : T extends TypeObject
+    ? ObjectSchema<C>
+    : never;
+
+  /**
+   * String Function
+   */
+
+  export const String = (): Schema<TypeString> => {
+    return {
+      _type: "string",
+      Optional: () => {
+        return {
+          _type: "optional",
+          child: {
+            _type: "string",
+          },
+        };
+      },
+    };
+  };
+
+  /**
+   * Number Function
+   */
+
+  export const Number = (): Schema<TypeNumber> => {
+    return {
+      _type: "number",
+      Optional: () => {
+        return {
+          _type: "optional",
+          child: {
+            _type: "number",
+          },
+        };
+      },
+    };
+  };
+
+  /**
+   * Boolean Function
+   */
+
+  export const Boolean = (): Schema<TypeBoolean> => {
+    return {
+      _type: "boolean",
+      Optional: () => {
+        return {
+          _type: "optional",
+          child: {
+            _type: "boolean",
+          },
+        };
+      },
+    };
+  };
+
+  /**
+   * Object Function
+   */
+
+  export const Construct = <C extends ChildrenObjectSchema>(
+    children: C
+  ): Schema<TypeObject, C> => {
+    return {
+      _type: "object",
+      children,
+      Optional: () => {
+        return {
+          _type: "optional",
+          child: {
+            _type: "object",
+            children,
+          },
+        };
+      },
+    };
+  };
+
+  /**
+   * Infer Type
+   */
 
   export type Infer<S extends Schema<Type> | OptionalSchema<Type>> =
-    S extends Schema<infer T, infer C>
-      ? T extends TypeString
-        ? string
-        : T extends TypeNumber
-        ? number
-        : T extends TypeBoolean
-        ? boolean
-        : T extends TypeObject
-        ? {
-            [K in keyof C]: Infer<C[K]>;
-          }
-        : never
+    S extends StringSchema
+      ? string
+      : S extends NumberSchema
+      ? number
+      : S extends BooleanSchema
+      ? boolean
+      : S extends ObjectSchema<infer C>
+      ? {
+          [K in keyof C]: Infer<C[K]>;
+        }
       : S extends OptionalSchema<infer T, infer C>
       ? T extends TypeString
         ? string | undefined
@@ -64,102 +203,15 @@ export namespace TSV {
             | undefined
         : never
       : never;
-
-  export const String = (): Schema<TypeString> => {
-    const defaultSchema: Pick<Schema<TypeString>, "_type" | "isOptional"> = {
-      _type: "string",
-      isOptional: false,
-    };
-
-    return {
-      ...defaultSchema,
-      Optional: () => {
-        defaultSchema.isOptional = true;
-        return {
-          ...defaultSchema,
-        };
-      },
-    };
-  };
-
-  export const Number = (): Schema<TypeNumber> => {
-    const defaultSchema: Pick<Schema<TypeNumber>, "_type" | "isOptional"> = {
-      _type: "number",
-      isOptional: false,
-    };
-
-    return {
-      ...defaultSchema,
-      Optional: () => {
-        defaultSchema.isOptional = true;
-        return {
-          ...defaultSchema,
-        };
-      },
-    };
-  };
-
-  export const Boolean = (): Schema<TypeBoolean> => {
-    const defaultSchema: Pick<Schema<TypeBoolean>, "_type" | "isOptional"> = {
-      _type: "boolean",
-      isOptional: false,
-    };
-
-    return {
-      ...defaultSchema,
-      Optional: () => {
-        defaultSchema.isOptional = true;
-        return {
-          ...defaultSchema,
-        };
-      },
-    };
-  };
-
-  export const Construct = <
-    S extends Record<string, Schema<Type> | OptionalSchema<Type>>
-  >(
-    children: S
-  ): Schema<TypeObject, S> => {
-    return {
-      _type: "object",
-      isOptional: false,
-      children: children,
-      Optional: () => {
-        return {
-          _type: "object",
-          isOptional: true,
-          children,
-        };
-      },
-    };
-  };
 }
 
 const user = TSV.Construct({
   id: TSV.Number(),
   first_name: TSV.String().Optional(),
-  last_name: TSV.String(),
-  confirmed: TSV.Boolean().Optional(),
-  location: TSV.Construct({
-    address_1: TSV.Construct({
-      number: TSV.Number(),
-      road: TSV.String(),
-      city: TSV.String(),
-      postal_code: TSV.Number(),
-    }).Optional(),
-    address_2: TSV.String(),
-  }),
 });
 
 type User = TSV.Infer<typeof user>;
 
-const user2: User = {
+const test: User = {
   id: 1,
-  last_name: "PICQUE",
-  confirmed: undefined,
-  location: {
-    address_1: undefined,
-    address_2: "test",
-  },
 };
